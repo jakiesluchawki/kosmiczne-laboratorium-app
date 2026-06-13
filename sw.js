c323cd459b25e9f156e7a65710f443eba9de953f
jakiesluchawki <mieszko@mahboob.pl>
2026-06-13 04:50:26 +0200

    deploy: publish verified application

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "kosmiczne-laboratorium-e3e21d1877805110";
+const CACHE = "kosmiczne-laboratorium-9ecce5440fe6b35d";
 const OFFLINE_URL = "./index.html";
 const PRECACHE_URLS = [
   "./404.html",
